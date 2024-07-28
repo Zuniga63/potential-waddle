f3223e3a2715ba9385cec9bb85b3bd8be22ee4bc
Andrés Felipe Zúñiga <andres.zuniga.063@gmail.com>
2024-07-27 20:50:12 -0500

    chore: restart database

--- src/modules/core/entities/category.entity.ts
+++ src/modules/core/entities/category.entity.ts
@@ -21,20 +21,14 @@ export class Category {
   @Column('text')
   name: string;
 
-  @Column('text', { name: 'name_en', nullable: true })
-  name_en?: string;
-
   @Column('text', { unique: true })
   slug: string;
 
-  @Column('text', { name: 'slug_en', unique: true, nullable: true })
-  slug_en?: string;
+  @Column('boolean', { name: 'is_enabled', default: false })
+  isEnabled: boolean;
 
   @Column('text', { nullable: true })
   description?: string;
-
-  @Column('text', { name: 'description_en', nullable: true })
-  description_en?: string;
 
   @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', name: 'created_at' })
   createdAt: Date;
