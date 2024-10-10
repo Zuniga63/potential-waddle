a3d39391357d2574b4a4562a76d3793ca948131a
Andrés Felipe Zúñiga <andres.zuniga.063@gmail.com>
2024-10-10 11:47:26 -0500

    style: apply linter to file

--- src/migrations/1727973024069-npm_config_name.ts
+++ src/migrations/1727973024069-npm_config_name.ts
@@ -1,14 +1,13 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class NpmConfigName1727973024069 implements MigrationInterface {
-    name = 'NpmConfigName1727973024069'
+  name = 'NpmConfigName1727973024069';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "place" ADD "popularity" smallint NOT NULL DEFAULT '0'`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "place" ADD "popularity" smallint NOT NULL DEFAULT '0'`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "place" DROP COLUMN "popularity"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "place" DROP COLUMN "popularity"`);
+  }
 }
